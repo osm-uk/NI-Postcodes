3622a4ae67b2d9d0e9723849f748363979680a7e
Cj Malone <CjMalone@mail.com>
2021-10-14 22:30:40 +0100

    Add excludes list
    A local list that we can use to ensure bad data dosen't get
    re added.

--- buildoutput.sh
+++ buildoutput.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
 cat data/*.csv | sort -ru > data/NI-postcodes.temp
+
+cat excludes.csv | while read line
+do
+#echo $line
+	sed -i "/$line/d" data/NI-postcodes.temp
+done
+
 mv data/NI-postcodes.temp data/NI-postcodes.csv
